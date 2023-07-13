28e3a8b1ae713b81fff4381f074d278713d54340
Cristian Navarro <cristian.navarro@jalasoft.com>
2023-07-13 04:35:37 -0500

    Add styles to post details

--- src/app/feed/post-details/post-details.component.ts
+++ src/app/feed/post-details/post-details.component.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable, map } from 'rxjs';
+import { Post } from 'src/app/core/models/post.model';
+import { PostService } from 'src/app/core/services/feed/post.service';
 
 @Component({
   selector: 'ngsocial-post-details',
   templateUrl: './post-details.component.html',
   styleUrls: ['./post-details.component.scss'],
 })
-export class PostDetailsComponent implements OnInit {
-  postId: string | null = null;
+export class PostDetailsComponent {
+  post$: Observable<Post> = this.route.data.pipe(
+    map(({ post }) => post as Post)
+  );
 
   constructor(private route: ActivatedRoute) {}
-
-  ngOnInit(): void {
-    this.route.paramMap.subscribe((params) => {
-      this.postId = params.get('postId');
-    });
-  }
 }
