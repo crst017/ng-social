49b607a8ac38814f163511d5b3a998d044903496
Cristian Navarro <cristian.navarro@jalasoft.com>
2023-07-13 01:03:43 -0500

    Save token on login

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,19 +1,37 @@
+import { isPlatformBrowser } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { Observable, tap } from 'rxjs';
 import { User } from 'src/app/core/models/user.model';
+
+interface TokenData {
+  access_token: string;
+}
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private readonly URL = 'http://localhost:3000';
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
   signup(userData: User): Observable<User> {
     return this.http.post<User>(`${this.URL}/auth/signup`, userData);
   }
 
-  login(userData: User): Observable<User> {
-    return this.http.post<User>(`${this.URL}/auth/login`, userData);
+  login(userData: User): Observable<TokenData> {
+    return this.http.post<TokenData>(`${this.URL}/auth/login`, userData).pipe(
+      tap((tokenData) => {
+        this.saveToken(tokenData['access_token']);
+      })
+    );
+  }
+
+  private saveToken(token: string): void {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('auth_token', token);
+    }
   }
 }
