b269f55cc4d1e61ecb510cd5eac6cd191b94e7ce
Cristian Navarro <cristian.navarro@jalasoft.com>
2023-07-13 01:08:48 -0500

    Add redirection to feed on login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,28 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../core/services/auth/auth.service';
 import { User } from '../core/models/user.model';
+import { Subject, takeUntil } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ngsocial-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   private readonly defaultValidators = [
     Validators.minLength(4),
     Validators.required,
   ];
+
+  private unsubscribe$ = new Subject<void>();
 
   loginForm = new FormGroup({
     username: new FormControl('', this.defaultValidators),
     password: new FormControl('', this.defaultValidators),
   });
 
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     this.loginForm.valueChanges.subscribe(console.log);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   login(): void {
@@ -30,6 +39,13 @@ export class LoginComponent implements OnInit {
       return;
     }
     const loginData = this.loginForm.value as User;
-    this.auth.login(loginData).subscribe();
+    this.auth
+      .login(loginData)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe({
+        next: () => {
+          this.router.navigate(['/feed']);
+        },
+      });
   }
 }
